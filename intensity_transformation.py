1bbaa59067945d055fa19cbf421b512794cab3b7
Victor R. Russo <vr.russo@usp.br>
2020-03-23 19:52:52 -0300

    provavelmente finalizado

--- intensity_transformation.py
+++ intensity_transformation.py
@@ -1,4 +1,4 @@
-#Victor Rodrigues Russo  11218855  SCC025 Image Processing  202001  intensity transformations
+#Victor Rodrigues Russo  11218855  SCC025 Image Processing  3º semester 2020  intensity transformations
 
 import numpy as np
 import imageio as im
@@ -14,19 +14,27 @@ def contrast_modulation(input_img,c,d):
     delta = float(d-c)/float(b-a)
     return ((input_img-a)*delta + c).astype(np.uint8)
 
+#transformation no 3 - applies the logarithmic function
+
+def logarithmic(input_img):
+    R = float(input_img.max())
+    return (255.0*(np.log2(1+input_img.astype(np.float))/np.log2(1+R))).astype(np.uint8)
+
 #transformation no 4 - applies a gamma adjustment to the image - returns the transformed image ready to write
 def gamma_adjustment(input_img,w,gamma):
-    R = 255.0 #the max gray level
-    return (np.power(w*(input_img.astype(np.float))/R,gamma)*R).astype(np.uint8)
+    return (np.power((input_img.astype(np.int32)),gamma)*w).astype(np.uint8)
 
 #evaluates the squared root error off the transformation - returns the error as a numpy float64
 def rse_evaluate(input_img,transformed_img):
-    size = input_img.shape
-    sum=0.0
-    for i in range(size[0]):
-        for j in range(size[1]):
-            sum += np.power((float(transformed_img[i][j])-float(input_img[i][j])),2)
-    return np.sqrt(sum)
+    #size = input_img.shape
+    #sum=0.0
+    #for i in range(size[0]):
+     #   for j in range(size[1]):
+      #      sum += np.power((float(transformed_img[i][j])-float(input_img[i][j])),2)
+    #return np.sqrt(sum)
+    error=np.zeros(transformed_img.shape).astype(np.float)
+    error=np.power(transformed_img.astype(np.float)-input_img.astype(np.float),2)
+    return np.sqrt(np.sum(error)).astype(np.float)
 
 
 #this is the main block of code
@@ -43,7 +51,7 @@ elif method == 2:
     d = int(input())
     transformed_img = contrast_modulation(input_img,c,d)
 elif method == 3:
-    print('logarithm function')
+    transformed_img = logarithmic(input_img)
 elif method == 4:
     w = int(input())
     gamma = float(input())
